436a150bac6b013454354a37d7e5c9d1482b4045
Eirik Baug <eirikbaug@hotmail.com>
2016-10-25 18:08:06 +1000

    ok

--- #Mood/public/javascripts/Javascript.js
+++ #Mood/public/javascripts/Javascript.js
@@ -2,42 +2,49 @@ var socket = io('http://localhost:3001');
 
 var negTweetCount = 0;
 var posTweetCount = 0;
-var nutTweetCount = 0;
-
+var UserId = 0;
+var precentage = '';
 socket.on('tweet', function (msg) {
     var tweet = '<amp-twitter width=600 height=850 layout="responsive" data-tweetid='+msg.tweetId+' data-cards="summary_large_image"> <blockquote id ="blockq" placeholder class="twitter-tweet" data-lang="en"> <p lang="en" dir="ltr" id="p"> <a id="l" class="list-group-item" href="https://twitter.com/'+msg.accountName+'/status/'+msg.tweetId+'">'+msg.tweet+'</a></p> </blockquote> </amp-twitter>';
-    if(msg.clas == 0) {
+    if(msg.clas == 0 && msg.userId == UserId) {
         negTweetCount += 1;
-        $('#negTweetsCount').val(negTweetCount);
+        precentagepos = (posTweetCount/(posTweetCount+negTweetCount))*100;
+        $('#precentage').text('Precentage positive: '+parseFloat(Math.round(precentagepos * 100) / 100).toFixed(2)+'%');
+        $('#negTweetCount').text(negTweetCount);
         twttr.widgets.createTweet(
           msg.tweetId,
             document.getElementById('negtweets'),{
               theme:'light'
         }
         );
-        //$('#negtweets').prepend($('<li>').innerHTML = tweet);
-    }else if(msg.clas == 4){
+    }else if(msg.clas == 4 && msg.userId == UserId){
         posTweetCount += 1;
-        $('#posTweetCount').val(posTweetCount);
+        precentagepos = (posTweetCount/(posTweetCount+negTweetCount))*100;
+        precentage = precentagepos +'%';
+        $('#precentage').text('Precentage positive: '+parseFloat(Math.round(precentagepos * 100) / 100).toFixed(2)+'%');
+        $('#posTweetCount').text(posTweetCount);
         twttr.widgets.createTweet(
             msg.tweetId,
             document.getElementById('postweets'),{
                 theme:'light'
             });
-
+    }
+});
+$('#posCheck').click(function(){
+    if (this.checked) {
+        $('#postweets').hide();
     }else{
-        nutTweetCount += 1;
-        $('#nutTweetsCount').val(nutTweetCount);
-        twttr.widgets.createTweet(
-            msg.tweetId,
-            document.getElementById('nuttweets'),{
-                theme:'light'
-            });
-
+        $('#postweets').show();
+    }
+});
+$('#negCheck').click(function(){
+    if (this.checked) {
+        $('#negtweets').hide();
+    }else{
+        $('#negtweets').show();
     }
 });
 $( document ).ready(function() {
-
-
+    UserId = $('#hiddenfield').val();
 });
 
--- #Mood/routes/index.js
+++ #Mood/routes/index.js
@@ -3,7 +3,53 @@ module.exports = function (io) {
     var router = express.Router();
     var Twit = require('twit');
     var natural = require('natural');
+    var randomstring = require("randomstring");
+    var MongoClient = require('mongodb').MongoClient
+        , assert = require('assert');
+    var url = 'mongodb://localhost:27017/Mood';
+    var fs = require('fs');
+    var classifierr = 0;
+    var started = false;
+    /*
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
 
+
+            var collection = db.collection('documents');
+            // Insert some documents
+            collection.insertMany([
+                {a : 1}, {a : 2}, {a : 3}
+            ]);
+
+        var mydocuments = fs.readFile('classifier.json', 'utf8', function (err, data) {
+            var collection = db.collection('classifier');
+            collection.insertOne(JSON.parse(data), function (err, docs) { // Should succeed
+                console.log('rumphull');
+                console.log(err);
+            });
+            db.close();
+        });
+
+            // Get the documents collection
+            var collection = db.collection('classifier');
+            // Find some documents
+            collection.find({}).toArray(function(err, docs) {
+                assert.equal(err, null);
+                assert.equal(1, docs.length);
+                console.log("Found the following records");
+                //console.dir(docs);
+                console.log(docs[0]);
+                natural.BayesClassifier.load(JSON.stringify(docs[0]), null, function(err, classifier) {
+
+                    classifierr = classifier;
+                });
+            });
+
+
+        console.log("tissefant");
+    });
+*/
     var T = new Twit({
         consumer_key:         '8uCnElCvIFMhfuAJbglIiMa2F',
         consumer_secret:      'U96g5T8vKT02dcpS1IJdC8u0jr5mr5CtfvoMyBRCenSJsefDLX',
@@ -11,48 +57,73 @@ module.exports = function (io) {
         access_token_secret:  'UYb6i3lUQ4yHXxH9q4ujlM6HHNVAn5mz6KjIKbzJNiasI'
     });
 
-    var stream = T.stream('statuses/filter', {track: ['#'+query]});
+    var stream = T.stream('statuses/filter', {language: 'en',track:['e']});
     /* GET home page. */
+
 
     router.get('/', function(req, res, next) {
 
         res.render('index', { title: '#Mood' });
     });
 
-    var classifierr = 0;
-    natural.BayesClassifier.load('classifier.json', null, function(err, classifier) {
-        classifierr = classifier;
-    });
 
     router.get("/search", function (req, res) {
+        if(io.engine.clientsCount > 0 && started ==false ){
+            stream.start();
+            started = true;
+            natural.BayesClassifier.load('classifier.json', null, function(err, classifier) {
+                classifierr = classifier;
+            });
+        }
         var query = req.query.q;
         natural.PorterStemmer.attach();
         console.log(query);
 
-        //console.log(classifierr.classify("icecream"));
+        var userId = randomstring.generate();
+        req.on('end',function () {
 
-        res.render('search', { title: '#'+query, search:query});
+        });
+        res.render('search', { title: query, userId:userId});
+
         stream.on('tweet', function (tweet) {
-            var accountname = tweet.user.screen_name;
-            var tweetid = tweet.id_str;
+            if(io.engine.clientsCount == 0){
+                stream.stop();
+                started = false;
+                classifierr.save('classifier.json', function(err, classifierr) {
+                    // the classifier is saved to the classifier.json file!
+                });
+
+            }else{
+            console.log(tweet.text);
+            var tweetxt = tweet.text;
+            if(tweetxt.includes(query)){
+                var accountname = tweet.user.screen_name;
+                var tweetid = tweet.id_str;
+                stemedList = tweetxt.tokenizeAndStem();
+                var sentence = "";
+                for(var i=0; i<stemedList.length;i++){
+                    sentence+= stemedList[i] +" ";
+                }
+                var clas = classifierr.classify(sentence);
+                classifierr.addDocument(sentence,clas);
+                var emit = {tweet: tweet, clas: clas,tweetId: tweetid, accountName: accountname,userId:userId};
+                io.emit('tweet',emit);
+            }
+            }
+            /*
+
             tweet = tweet.text;
             //console.log(tweet);
-            stemedList = tweet.tokenizeAndStem();
-            var sentence = "";
-            for(var i=0; i<stemedList.length;i++){
-                sentence+= stemedList[i] +" ";
-            }
 
-            var clas = classifierr.classify(sentence);
-            //for(var j=0;j<stemedList.length;j++){
-            classifierr.addDocument(sentence,clas);
-            classifierr.save('classifier.json', function(err, classifierr) {
+                        //for(var j=0;j<stemedList.length;j++){
+            //classifierr.addDocument(sentence,clas);
+            //classifierr.save('classifier.json', function(err, classifierr) {
                 // the classifier is saved to the classifier.json file!
             });
             var emit = {tweet: tweet, clas: clas,tweetId: tweetid, accountName: accountname};
             //}
             //console.log(clas);
-            io.emit('tweet',emit);
+            */
 
 
         });
